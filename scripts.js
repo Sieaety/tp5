71343018d86718a5012f1d1a1832106d5d17bdba
Sieaety <kzorncom@badbrains.reclaimhosting.com>
2021-03-07 22:38:11 +0000

    added javascript to make list visibility  toggle

--- scripts.js
+++ scripts.js
@@ -4,4 +4,15 @@ window.onload = function() {
   x.addEventListener("click", function() {
     x.className = "changeColor";
   });
+  list = document.querySelectorAll("h4");
+  console.log(list);
+  list.item(0).addEventListener("click", function() {
+    document.querySelector('#ingredients').classList.toggle('hide');
+  });
+    list.item(1).addEventListener("click", function() {
+    document.querySelector('#equipment').classList.toggle('hide');
+  });
+    list.item(2).addEventListener("click", function() {
+    document.querySelector('#directions').classList.toggle('hide');
+  });
 };
